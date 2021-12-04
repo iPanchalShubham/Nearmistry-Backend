a1182355519f30121a4aa15261f0e6de6172de0e
Shubham Panchal <panchalshubham@826gmail.com>
2021-12-04 22:53:48 +0530

    corrected index line no 38

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"));
     app.get('*',(NODE_ENV,res)=>{
-        res.sendFile(path.resolve(__dirname,"client,build",'index.html'));
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
 }
 
