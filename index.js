14edf5db0be67a0a288c22f6e505f5786fc73644
Shubham Panchal <panchalshubham@826gmail.com>
2021-12-07 15:18:15 +0530

    did same thing again

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ mongoose.connect(process.env.MONGODB_URI||db,{
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 if(process.env.NODE_ENV == "production"){
-    app.use('/volunteerSection',routes)
     app.get('/*', function(_, res) {
         res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
     });
+    app.use('/volunteerSection',routes)
 }
 
 app.listen(process.env.PORT||5000,()=>console.log(`Application Running on port ${process.env.PORT}`))
