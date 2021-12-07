462f3412c47d3597ac5a601e86eb6d15b8945b25
Shubham Panchal <panchalshubham@826gmail.com>
2021-12-07 15:23:11 +0530

    changed index.js

--- index.js
+++ index.js
@@ -33,10 +33,11 @@ mongoose.connect(process.env.MONGODB_URI||db,{
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 if(process.env.NODE_ENV == "production"){
-    app.get('/*', function(_, res) {
-        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
-    });
+    app.use(express.static("client/build"));
     app.use('/volunteerSection',routes)
+    app.get('/*', function(_, res) {
+        res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+    });
 }
 
 app.listen(process.env.PORT||5000,()=>console.log(`Application Running on port ${process.env.PORT}`))
