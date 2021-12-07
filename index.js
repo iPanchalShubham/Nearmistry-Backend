b30155c4b6ce290905b1e0cb153f4d710c25ba99
Shubham Panchal <panchalshubham@826gmail.com>
2021-12-07 13:41:33 +0530

    changed index.js

--- index.js
+++ index.js
@@ -35,10 +35,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"));
     const serverPostPath = path.join(__dirname, 'appRoute/routes.js'); 
-    app.use('/volunteerSection',serverPostPath)
-    app.get('*',(NODE_ENV,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
+    app.use('/volunteerSection')
 }
 
 app.listen(process.env.PORT||5000,()=>console.log(`Application Running on port ${process.env.PORT}`))
