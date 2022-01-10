9d6e89b8fab914b3e728c7876ab26286a3d539e7
Shubham Panchal <panchalshubham@826gmail.com>
2022-01-10 13:59:30 +0530

    fixed bug --> Added occupation query in client/api/index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ const url = 'https://labrecruit.herokuapp.com/'
 //https is very important, because if you make request to change something through a http protocol (unsecure network) then your request 
 // could be blocked.
 //Reponse data coming from getHomePage Funtion from our server side.
-export const fetchUsers = (page,filterVars) => axios.get(url+`volunteerSection/homePage?page=${page}&helper=${filterVars.helper}&raj_mistri=${filterVars.raj_mistri}&labour=${filterVars.labour}&painter=${filterVars.painter}&welder=${filterVars.welder}&tileGraniteWorkers=${filterVars.tileGraniteWorkers}`)
+export const fetchUsers = (page,filterVars) => axios.get(url+`volunteerSection/homePage?page=${page}&helper=${filterVars.helper}&raj_mistri=${filterVars.raj_mistri}&labour=${filterVars.labour}&painter=${filterVars.painter}&welder=${filterVars.welder}&tileGraniteWorkers=${filterVars.tileGraniteWorkers}&occupation=${nothing}`)
 //Send data to server-side
 export const createUser = (newUserData) => axios.post(url+'volunteerSection/newUser',newUserData)
 //Create new Volunteer
