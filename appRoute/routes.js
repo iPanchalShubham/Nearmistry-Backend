42be08b2d9e000e28cfad9067abaaced1932d717
Shubham Panchal <panchalshubham@826gmail.com>
2022-01-12 15:49:42 +0530

    Added new endPoint of getInfo

--- appRoute/routes.js
+++ appRoute/routes.js
@@ -2,11 +2,12 @@ import express from 'express'
 import {getHomePage} from '../controllers/controllers.js'
 import {postNewUser} from '../controllers/controllers.js'
 import { postNewVolunteer } from '../controllers/controllers.js';
+import { getInfo } from '../controllers/controllers.js';
 //Router 
 const router = express.Router();
 
 router.get('/homePage',getHomePage)
 router.post('/newUser',postNewUser)
 router.post('/newVolunteer',postNewVolunteer)
-
+router.get('/homePage/getInfo',getInfo)
 export default router
